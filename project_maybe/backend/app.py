d1955209087c8d73218c97d99cbe99b254265ed4
SheetsC <120830069+SheetsC@users.noreply.github.com>
2023-04-24 14:29:40 -0500

    Add files via upload
    initial commit. migrations, backend-frontend set up done

---
+++ project_maybe/backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate( app, db)
