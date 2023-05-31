7d60f7acf6fc0ef189bc8759294cb968b5a4b401
Connor Sheets <connorsheets32@gmail.com>
2023-05-31 10:50:14 -0500

    trying to add host

--- project_maybe/backend/app.py
+++ project_maybe/backend/app.py
@@ -372,4 +372,4 @@ import os
 port = os.environ.get('5555')
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5555))
-    app.run(port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
